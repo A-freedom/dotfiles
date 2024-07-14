08ffba825859798d468a235861ab4062aa0d1d01
A-freedom <ali.musa.omran+github@gmail.com>
2024-07-14 09:59:00 +0300

    update isntall.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function install_i3_gaps() {
+
   mkdir /tmp/build 
   cd /tmp/build
   git clone https://www.github.com/Airblader/i3 i3-gaps
@@ -27,50 +28,28 @@ echo "Updating and upgrading the system..."
 sudo apt update
 sudo apt upgrade -y
 
-# Packages to install
-packages=()
 
 # Check if the user wants to install each package
-for program in git curl stow zsh neovim code playerctl feh diodon compton polybar alacritty i3; do
-    if prompt_install "$program"; then
-        packages+=("$program")
-    fi
-done
-
-# Install selected packages
-if [ ${#packages[@]} -ne 0 ]; then
-    sudo apt install "${packages[@]}" -y
-fi
-
+sudo apt install git curl stow zsh code playerctl feh diodon compton polybar alacritty i3
 # Make Zsh the default shell
-if prompt_install "zsh as default shell"; then
-    chsh -s $(which zsh)
-fi
+chsh -s $(which zsh)
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#
+# Install Zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
+
+
 
 
 # Copy dotfiles to the system
-if prompt_install "dotfiles using stow"; then
-    stow .
-fi
 
-# Remap keyboard layout
-if prompt_install "keyboard layout remapping"; then
-    sudo bash -c 'cat << EOF > /etc/X11/xorg.conf.d/00-keyboard.conf
-Section "InputClass"
-    Identifier "system-keyboard"
-    MatchIsKeyboard "on"
-    Option "XkbOptions" "terminate:ctrl_alt_bksp,caps:escape_shifted_capslock"
-EndSection
-EOF'
-fi
+stow .
 
-# Change the functionality of the power button
-if prompt_install "power button functionality change"; then
-    sudo sed -i 's/^#\(HandlePowerKey=\).*/\1suspend/' /etc/systemd/logind.conf
-    sudo sed -i 's/^#\(HandlePowerKeyLongPress=\).*/\1poweroff/' /etc/systemd/logind.conf
-    sudo sed -i 's/^\(HandlePowerKey=\).*/\1suspend/' /etc/systemd/logind.conf
-    sudo sed -i 's/^\(HandlePowerKeyLongPress=\).*/\1poweroff/' /etc/systemd/logind.conf
-fi
-
-echo "Installation and configuration completed. Please restart your computer for all changes to take effect."
 
