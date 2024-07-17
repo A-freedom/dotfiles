3eee52660a0129785dbc7724433a7b4203a26790
A-freedom <ali.musa.omran+github@gmail.com>
2024-07-17 12:59:14 +0300

    neovim config update 1. changing the defualt behavior of copying the text when using `c` or `d` to just deleting the text 2. some movement remaping   1.change the leader from `space` to `z`   2.making shortcuts form moving to the start or the end of a line or a page using the `<leader> + [h,j,k,l]` 3. enable relative lines numbering

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,44 @@
+-- disable the auto copying
+vim.api.nvim_set_keymap('n', 'd', '"_d', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'D', '"_D', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'c', '"_c', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'C', '"_C', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'd', '"_d', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'D', '"_D', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'c', '"_c', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'C', '"_C', { noremap = true, silent = true })
+
+vim.g.mapleader = "z"
+
+-- Remap to go to the start of the line
+vim.api.nvim_set_keymap('n', '<leader>h', '^', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>h', '^', { noremap = true, silent = true })
+
+-- Remap to go to the end of the line
+vim.api.nvim_set_keymap('n', '<leader>l', '$', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>l', '$', { noremap = true, silent = true })
+
+-- Remap to go to the start of the file
+vim.api.nvim_set_keymap('n', '<leader>k', 'gg', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>k', 'gg', { noremap = true, silent = true })
+
+-- Remap to go to the end of the file
+vim.api.nvim_set_keymap('n', '<leader>j', 'G', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>j', 'G', { noremap = true, silent = true })
+
+-- Enable both normal and relative line numbering
+vim.api.nvim_exec([[
+  set number       " Enable normal line numbering
+  set relativenumber  " Enable relative line numbering
+]], false)
+
+-- Set number of context lines (optional)
+vim.api.nvim_exec([[
+  set numberwidth=2   " Adjust the width of line numbers if needed
+]], false)
+
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
-vim.g.mapleader = " "
+-- vim.g.mapleader = " "
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
@@ -12,7 +51,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local lazy_config = require "configs.lazy"
-
 -- load plugins
 require("lazy").setup({
   {
@@ -37,3 +75,6 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+
+
